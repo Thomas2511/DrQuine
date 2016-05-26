d013016f9bbc92e31c073e3c54474cdb79b5fe7f
Thomas Dieumegard <tdieumeg@student.42.fr>
2016-05-26 18:44:04 +0200

    pouet

--- Colleen.c
+++ Colleen.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /*
-   commentaire
+	commentaire
 */
 void f(char *s)
 {
-	printf(s,10,10,10,10,10,10,9,10,10,10,10,10,9,10,9,10,9,10,9,34,s,34,10,9,10,9,10,10);
+	printf(s,10,10,9,10,10,10,10,9,10,10,10,10,10,9,10,9,9,10,9,10,9,34,s,34,10,9,10,9,10,10);
 }
 
 int main()
 {
 	/*
-	   commentaire
+		commentaire
 	*/
-	char *s = "#include <stdio.h>%c/*%c   commentaire%c*/%cvoid f(char *s)%c{%c%cprintf(s,10,10,10,10,10,10,9,10,10,10,10,10,9,10,9,10,9,10,9,34,s,34,10,9,10,9,10,10);%c}%c%cint main()%c{%c%c/*%c%c   commentaire%c%c*/%c%cchar *s = %c%s%c;%c%cf(s);%c%creturn (0);%c}%c";
+	char *s = "#include <stdio.h>%c/*%c%ccommentaire%c*/%cvoid f(char *s)%c{%c%cprintf(s,10,10,9,10,10,10,10,9,10,10,10,10,10,9,10,9,9,10,9,10,9,34,s,34,10,9,10,9,10,10);%c}%c%cint main()%c{%c%c/*%c%c%ccommentaire%c%c*/%c%cchar *s = %c%s%c;%c%cf(s);%c%creturn (0);%c}%c";
 	f(s);
 	return (0);
 }
