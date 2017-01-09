5c10c43caf511be35c4874e916816f8bd49b0947
Thomas Dieumegard <tdieumeg@student.42.fr>
2017-01-09 16:32:11 +0100

    Grace done

--- Grace.c
+++ Grace.c
@@ -1,6 +1,10 @@
-#define F() int main() {
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 /*
 	commentaire
 */
-FILE *fd = write return (0); }
-F()
+#define CHAR(x) char *s = #x; x
+#define OPEN *stream = fopen("Grace_kid.c", "w+")
+#define MAIN() int main() { FILE OPEN; CHAR(fprintf(stream, "#include <stdio.h>\n#include <fcntl.h>\n#include <unistd.h>\n/*\n\tcommentaire\n*/\n#define CHAR(x) char *s = #x; x\n#define OPEN *stream = fopen(\"Grace_kid.c\", \"w+\")\n#define MAIN() int main() { FILE OPEN; CHAR(%s); }\nMAIN()\n", s)); }
+MAIN()
